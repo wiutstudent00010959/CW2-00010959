bb5f8c44db550d063aeec8b39e97bfb539ced1bb
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2022-03-31 22:10:06 +0500

    frontend + part of backend

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const app = express()
+const PORT = 3000
 
+app.set('view engine', 'pug')
+app.use('/static', express.static('assets')) //assets
+app.use(express.urlencoded({extended: false}))
+
+//localhost:8000
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.post('/add', (req, res) => {
+    const formData = req.body
+
+    if formData.todo.trim() =='') {
+        res.render('home', { error: true})
+        //if there is no input, send error
+    } else {
+        
+    }
+}) 
+
+//app.listen(PORT, (err) => {
+//    if (err) throw err
+//    console.log(`This app is running on port ${PORT}`)
+//})

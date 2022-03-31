d7cdbdd203a616c35f6be9fe243a9a1b0b47cbc3
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2022-03-31 22:22:47 +0500

    more backend

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+//third party libraries
 const express = require('express');
 const app = express()
+
+//node libraries
+const fs = require('fs')
 const PORT = 3000
 
 app.set('view engine', 'pug')
@@ -13,12 +17,30 @@ app.get('/', (req, res) => {
 
 app.post('/add', (req, res) => {
     const formData = req.body
-
-    if formData.todo.trim() =='') {
+//if there is no input, send error
+    if (formData.todo.trim() =='') {
         res.render('home', { error: true})
-        //if there is no input, send error
-    } else {
         
+    } else { //else try reead dtatabse file, if not possible, throw error
+        FileSystem.readFile('./data/todos.json', (err, data)) => {
+            if (err) throw err
+            const todo = {
+                id: id(),
+                description: formData.todo,
+                done: false //this will serve as property namewhen CRUD is implemented
+            }
+            todos.push(todo)
+
+            fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+                if (err) throw err
+                
+                res.render('home', { success: true} )
+            }
+            
+            )
+        }
+   
+
     }
 }) 
 
@@ -26,3 +48,11 @@ app.post('/add', (req, res) => {
 //    if (err) throw err
 //    console.log(`This app is running on port ${PORT}`)
 //})
+
+
+function id() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
